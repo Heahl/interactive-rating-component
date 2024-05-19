9267abf7c4d0c9fd58fae6c15a13450a4f3b7f20
heahl <hendrik712.a@gmail.com>
2024-05-19 14:28:48 +0200

    final commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,21 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      colors: {
+        'orange' : 'hsl(25, 97%, 53%)',
+        'light-grey' : 'hsl(217, 12%, 63%)',
+        'dark-blue' : 'hsl(213, 19%, 18%)',
+        'very-dark-blue' : 'hsl(216, 12%, 8%)'
       },
+      fontFamily: {
+        sans: ["Overpass", "var(--font-geist-sans)", ...fontFamily.sans],
+        },
     },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['hover'],
+    }
   },
   plugins: [],
 } satisfies Config;
